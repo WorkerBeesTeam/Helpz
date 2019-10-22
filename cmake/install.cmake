f1bc9de0f3c9444a940947154cc19ca5dce0e4a4
Maksim <maksim.lv@mail.ru>
2019-10-22 04:24:49 +0300

    Building with debs

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -4,6 +4,23 @@
 
 install(TARGETS ${TARGET} DESTINATION lib)
 
-add_custom_command(TARGET ${TARGET}
-        POST_BUILD
-        COMMAND checkinstall -D --pkgname=lib${TARGET} --pkgversion=${PROJECT_VERSION_STRING} --bk --nodoc --default --install=no --pakdir=../ > checkinstall.log)
+if(REQUIRED_PKGS OR REQUIRED_PKGS_QT)
+
+    message("PKGS: ${REQUIRED_PKGS} / QT: ${REQUIRED_PKGS_QT}")
+
+    if (REQUIRED_PKGS AND REQUIRED_PKGS_QT)
+        set(REQUIRED_PKGS "${REQUIRED_PKGS_QT},${REQUIRED_PKGS}")
+    elseif(REQUIRED_PKGS_QT)
+        set(REQUIRED_PKGS ${REQUIRED_PKGS_QT})
+    endif()
+
+    message("SUMMARY: ${REQUIRED_PKGS}")
+
+    set(REQUIRES_STR "--requires=\"${REQUIRED_PKGS}\"")
+endif()
+
+message("checkinstall -D --pkgname=lib${TARGET} --pkgversion=${PROJECT_VERSION_STRING} ${REQUIRES_STR} --bk --nodoc --default --install=no --pakdir=../")
+
+add_custom_target(${TARGET}-deb
+        COMMAND checkinstall -D --pkgname=lib${TARGET} --pkgversion=${PROJECT_VERSION_STRING} ${REQUIRES_STR} --bk --nodoc --default --install=no --pakdir=../
+        DEPENDS ${TARGET})
